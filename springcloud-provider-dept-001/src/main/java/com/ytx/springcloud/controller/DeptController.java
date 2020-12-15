7c9845f4ce4e0e50fd61920aa2551fee0e19c19a
iiinvoker <478117100@qq.com>
2020-12-15 12:05:06 +0800

    引入eureka

--- springcloud-provider-dept-001/src/main/java/com/ytx/springcloud/controller/DeptController.java
+++ springcloud-provider-dept-001/src/main/java/com/ytx/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.ytx.springcloud.controller;
 import com.ytx.springcloud.pojo.Dept;
 import com.ytx.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,11 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+
+    //得到具体微服务的信息
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
 
     @PostMapping("dept/add")
     public boolean addDept(Dept dept){
@@ -28,5 +35,19 @@ public class DeptController {
     @GetMapping("/dept/list")
     public List<Dept> queryAll(){
         return deptService.queryAll();
+    }
+
+    @GetMapping("/dept/discovery")
+    public Object discovery(){
+        //获的微服务列表的清单
+        List<String> services = discoveryClient.getServices();
+        System.out.println("discovery=>service"+services);
+        List<ServiceInstance> instances=discoveryClient.getInstances("springcloud-provider-dept");
+        for (ServiceInstance instance:instances) {
+            System.out.println(
+                    instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri()+"\t"+instance.getServiceId()
+            );
+        }
+        return this.discoveryClient;
     }
 }
